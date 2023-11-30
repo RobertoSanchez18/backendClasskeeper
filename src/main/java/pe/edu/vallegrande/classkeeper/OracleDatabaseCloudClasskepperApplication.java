87509aade5810a4751001c473950e7e9e8869e3a
RobertoSanchez18 <roberto.sanchez@vallegrande.edu.pe>
2023-11-30 13:39:18 -0500

    Backend

---
+++ src/main/java/pe/edu/vallegrande/classkeeper/OracleDatabaseCloudClasskepperApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.classkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleDatabaseCloudClasskepperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracleDatabaseCloudClasskepperApplication.class, args);
+	}
+
+}
